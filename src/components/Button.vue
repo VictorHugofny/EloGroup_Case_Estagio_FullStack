7b0aff701272cd7920b9a62bf27bd37b34965369
Victor Hugo <victorhugofny@gmail.com>
2021-10-28 23:49:37 -0300

    Initial commit

---
+++ src/components/Button.vue
@@ -0,0 +1,15 @@
+<template>
+    <div class = "Button">
+        <button type="submit">Registrar</button>
+    </div>
+</template>
+
+<style>
+.Button button{
+    padding: 10px;
+    margin-top: 20px;
+    width: 180px;
+    background: white;
+    border: 1px solid black;
+}
+</style>
---
+++ src/components/Header.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="Header">
+      <img src="../assets/logocopy.png" >
+  </div>
+</template>
+
+<style>
+.Header img{
+    height: 50px;
+}
+</style>
